c1742850d1c8dd737cd52f41f795ed79d7a6fc0f
Adam Miner <miner.adam1@hotmail.com>
2018-02-13 13:17:58 -0700

    Tweaks

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var wins = document.getElementsByClassName("wins");
+var losses = document.getElementsByClassName("losses");
+var remaining = document.getElementsByClassName("remaining");
+var word = document.getElementsByClassName("word");
+var guessed = document.getElementsByClassName("guessed");
+
